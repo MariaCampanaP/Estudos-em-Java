6e704c16fe3e0d8ee3fa6f78b9d80f035929af7a
Maria <mariacampana121@gmail.com>
2025-04-29 12:40:46 -0300

    Add files via upload

---
+++ Escola/Aluno.java
@@ -0,0 +1,12 @@
+package com.mycompany.escola;
+
+public class Aluno {
+   String nome;
+   double nota;
+   
+   void exibirDados(){
+       System.out.println("Nome do aluno:" + this.nome);
+       System.out.println("Nota do aluno:" + this.nota);
+    }
+   
+}
