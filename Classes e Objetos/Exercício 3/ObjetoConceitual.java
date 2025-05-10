3a04f8d0f445da562082e95e0cb68be5c2e5869c
Maria <mariacampana121@gmail.com>
2025-05-09 21:58:54 -0300

    Add files via upload

---
+++ Classes e Objetos/Exercício 3/ObjetoConceitual.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.objetoconceitual;
+
+/**
+ *
+ * @author maria
+ */
+public class ObjetoConceitual {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
---
+++ Testes/Exercício 3/Iluminacao.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.iluminacao;
+
+/**
+ *
+ * @author maria
+ */
+public class Iluminacao {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
